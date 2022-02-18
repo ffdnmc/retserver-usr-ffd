e5c7a4dddc2f7af8e2184dedb3922c8d8cea2e3a
Francisco Florez <ffdnmc@gmail.com>
2022-02-17 21:06:14 -0500

    Actualizacion 17/02/2022 noche

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,4 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 
 
@@ -11,14 +11,12 @@ const usuariosGet = async(req = request, res = response) => {
     const { limite = 5, desde = 0 } = req.query;
     const query = { estado: true };
 
-    // ejecuta las promesas simultaneamente y solo retorna cuando ambas terminen
-    const [ total, usuarios  ] = await Promise.all([
-        Usuario.count(query),
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
         Usuario.find(query)
-            .skip(desde)
-            .limit(Number(limite))
+            .skip( Number( desde ) )
+            .limit(Number( limite ))
     ]);
-
 
     res.json({
         total,
@@ -26,69 +24,65 @@ const usuariosGet = async(req = request, res = response) => {
     });
 }
 
-const usuariosPost = async(req, res) => {
-
+const usuariosPost = async(req, res = response) => {
+    
     const { nombre, correo, password, rol } = req.body;
     const usuario = new Usuario({ nombre, correo, password, rol });
 
-    // verificar si el correo existe
-    // Debe hacerse en en las validadiones de la base  ../db-validators.js
-
-    // Encriptar contraseña
+    // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
     usuario.password = bcryptjs.hashSync( password, salt );
-    // Guardar en base de datos
+
+    // Guardar en BD
     await usuario.save();
-
-    res.json({
-        usuario       
-    });
-}
-
-const usuariosPut = async(req, res = response) => {
-
-    const { id } = req.params;
-    const { _id, pasword, google, correo, ...resto } = req.body;
-
-    // TODO: validar id exista en DB
-    if ( pasword ) {
-        // Encriptar contraseña
-        const salt = bcryptjs.genSaltSync();
-        resto.password = bcryptjs.hashSync( password, salt );       
-    }
-
-    const usuario = await Usuario.findByIdAndUpdate(id, resto)
-
-    res.json( usuario );
-}
-
-const usuariosPatch = (req, res) => {
-
-    res.json({
-        msg: 'patch API - usuariosPatch'
-    });
-}
-
-const usuariosDelete = async(req, res=response) => {
-
-    const { id } = req.params;
-
-    // // Borrar fisicamente
-    // const usuario = await Usuario.findByIdAndDelete( id );
-
-    // Cambiando estado del usuario
-    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } );
-
 
     res.json({
         usuario
     });
 }
 
+const usuariosPut = async(req, res = response) => {
+
+    const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
+
+    if ( password ) {
+        // Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync( password, salt );
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate( id, resto );
+
+    res.json(usuario);
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    });
+}
+
+const usuariosDelete = async(req, res = response) => {
+
+    const { id } = req.params;
+
+    // Fisicamente lo borramos
+    // const usuario = await Usuario.findByIdAndDelete( id );
+
+    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } );
+
+
+    res.json(usuario);
+}
+
+
+
+
 module.exports = {
     usuariosGet,
     usuariosPost,
     usuariosPut,
     usuariosPatch,
-    usuariosDelete
+    usuariosDelete,
 }
